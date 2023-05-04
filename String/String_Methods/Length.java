72854614a7f390c0b365f79c28822f8a138387db
virajjagtap2003 <jagtapviraj2003@gmail.com>
2023-05-04 19:17:27 +0000

    Replace Length.java

--- String/String_Methods/Length.java
+++ String/String_Methods/Length.java
@@ -1,14 +1,21 @@
 class LengthDemo{
+
+	static int mystrlen (String str){
+		char arr[]=str.toCharArray();
+		int count=0;
+		for(int i=0;i<arr.length;i++){
+			count++;
+		}
+		return count;
+	}
 	public static void main(String []args){
-		String str="viraj";
-		System.out.println(str.length());
+		/*String str="viraj";
+		System.out.println(str.length());*/
 
 		// Length without using length method
-		int i=0;
-		for(char c:str.toCharArray()){
-			i++;
-		}
-		System.out.println("Length="+i);
 		
+		String str="Viraj";
+		int len=mystrlen(str);
+		System.out.println(len);
 	}
 }
