06bbacbe6f901944694d70421d6aa1e03c16d357
virajjagtap2003 <jagtapviraj2003@gmail.com>
2023-07-25 12:42:31 +0530

    ArrayList,Multithreading

---
+++ Multithreading/MethodJoin.java
@@ -0,0 +1,30 @@
+class Mythread extends Thread{
+	public void run(){
+	
+		for(int i=1;i<=10;i++)
+		{
+			
+			try{
+				Thread.sleep(2000);
+			}catch(InterruptedException obj3){
+			}
+				System.out.println("In thread-0");
+		}
+	}
+
+}
+class ThreadDemo{
+	public static void main(String[] args){
+		Mythread obj=new Mythread();
+		obj.start();
+		try{
+			
+			obj.join();  // jya class la join kely to aadhi nantrr dusra sleep to sleep asla tri
+		}catch(InterruptedException obj1){
+		}
+
+		for(int i=1;i<=10;i++){
+			System.out.println("In main");
+		}
+	}
+}
---
+++ Multithreading/MethodSleep.java
@@ -0,0 +1,39 @@
+class Mythread extends Thread {
+
+	void fun(){
+	/*	try{
+			Thread.sleep(5000);
+		}catch(InterruptedException obj3){
+		}*/
+		System.out.println("hello");
+	}
+	public void run()/*throws InterruptedException*/{ /*only try catch is used in run method*/
+		try{
+			Thread.sleep(3000);
+		}catch(InterruptedException obj4){
+		}
+		System.out.println(Thread.currentThread());
+		// It shows Thread[Thread-0,5,main]==Thread-0(current thread)5(Default priority),main=running thread
+	}
+}
+class ThreadDemo{
+	public static void main(String[] args){
+		Mythread obj=new Mythread();
+		//obj.setPriority(7);
+		obj.fun();
+		obj.start();
+		try{
+			Thread.sleep(2000);
+		}catch(InterruptedException obj1){
+
+			
+		}
+		System.out.println(Thread.currentThread()); // getname shows here "main"
+		// It shows Thread[main,5,main]==main(current thread)5(Default priority),main=running thread
+		
+		// SetName method to set threadname
+		Thread.currentThread().setName("Viraj");
+
+		System.out.println(Thread.currentThread()); 
+	}
+}
